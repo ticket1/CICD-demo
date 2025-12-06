1d213ec426839b5b3ebc612b798446b3e7855aec
ticket1 <karischan0617@gmail.com>
2025-12-06 20:33:49 +0900

    json update

--- playwright.config.js
+++ playwright.config.js
@@ -1,7 +1,13 @@
-const config = {
+const { defineConfig } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './tests',
+  /* 這裡很重要：同時產生 HTML 報告 (playwright-report) 和 Allure 資料 (allure-results) */
   reporter: [
-    ['line'],
-    ['allure-playwright']
+    ['html'],             // 這是給 GitHub Actions 上傳 artifact 用的
+    ['allure-playwright'] // 這是給 Allure Report 用的
   ],
-};
-module.exports = config;
+  use: {
+    trace: 'on-first-retry',
+  },
+});
